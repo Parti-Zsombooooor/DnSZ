906557adc514de448f37179070800a91aff24c7a
Parti Zsombor <partizsombor@turr.hu>
2026-05-18 11:30:34 +0200

    GeneralasKodeStared

--- DnSZ/Form1.Designer.cs
+++ DnSZ/Form1.Designer.cs
@@ -41,10 +41,12 @@
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 9;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(157, 160);
+            this.listBox1.Size = new System.Drawing.Size(265, 256);
             this.listBox1.TabIndex = 0;
             // 
             // AjtoFel
--- DnSZ/Form1.cs
+++ DnSZ/Form1.cs
@@ -12,9 +12,70 @@ namespace DnSZ
 {
     public partial class Form1 : Form
     {
+        int[,] matrix = new int[25, 25];
+
         public Form1()
         {
             InitializeComponent();
+            MatrixFeltoltese();
+            SzobaGeneralasa();
+            MatrixMegjelenitese();
+        }
+
+        private void SzobaGeneralasa()
+        {
+            int szobaSzam = 0;
+            int jelelegiHelyX = 12;
+            int jelelegiHelyY = 24;
+            matrix[12, 24] = 1;
+
+            while(szobaSzam <= 6)
+            {
+                Random rd = new Random();
+                int irany = rd.Next(0, 3);
+                if(irany == 1)
+                {
+
+                }
+                else if(irany == 2)
+                {
+
+                }
+                else if (irany == 3)
+                {
+
+                }
+
+            }
+            
+        }
+
+        private void MatrixMegjelenitese()
+        {
+            listBox1.Items.Clear();
+            for (int sor = 0; sor < 25; sor++)
+            {
+                string aktualisSor = "";
+                for (int oszlop = 0; oszlop < 25; oszlop++)
+                {
+                    aktualisSor += matrix[sor, oszlop].ToString().PadLeft(4);
+                }
+                listBox1.Items.Add(aktualisSor);
+            }
+        }
+
+        private void MatrixFeltoltese()
+        {
+            
+            for (int sor = 0; sor < 26; sor++)
+            {
+                for (int oszlop = 0; oszlop < 26; oszlop++)
+                {
+                    matrix[sor, oszlop] = 0;
+                }
+            }
+
+            
         }
     }
 }
